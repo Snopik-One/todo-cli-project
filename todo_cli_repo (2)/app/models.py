1180ea63c535ca0fc63ff45ed9e620781a46845e
Snopik-One <98995795+Snopik-One@users.noreply.github.com>
2026-06-15 03:04:01 +0900

    Delete todo_cli_repo (2) directory

--- todo_cli_repo (2)/app/models.py
+++
@@ -1,6 +0,0 @@
-# Simple structure placeholder
-class Task:
-    def __init__(self, id, title, done=False):
-        self.id = id
-        self.title = title
-        self.done = done
--- todo_cli_repo (2)/tests/test_logic.py
+++
@@ -1,6 +0,0 @@
-from app.logic import add_task
-
-def test_add_task():
-    tasks = []
-    add_task(tasks, "test")
-    assert len(tasks) == 1
